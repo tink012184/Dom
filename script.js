3d52416a39afb59da92ef5fe0802bb5018d6bed6
Tink <85289337+tink012184@users.noreply.github.com>
2024-04-10 20:46:44 -0600

    Pictures

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-var slideIndex = 0;
-var slides = ["steampunk_fairy.jpg", "dumpster.JPG", "tink.PNG", "unicorn.JPG"]; // Add your images to this array
-var captions = ["Steampunk Fairy", "Dumpster", "Tinkrebell", "Unicorn"]; // Add your captions to this array
+let slideIndex = 0;
+const slides = ["steampunk_fairy.jpg", "tink.PNG", "dumpster.jpg", "unicorn.JPG"]; // Replace with your image paths
+const slideImage = document.getElementById('slide');
 
 function showSlides() {
-  var i;
-  var slideImage = document.getElementById("slide-image");
-  var captionText = document.querySelector(".text");
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1} 
-  slideImage.src = slides[slideIndex-1];
-  captionText.innerHTML = captions[slideIndex-1];
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slideImage.src = slides[slideIndex - 1];
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
 
-showSlides();
+showSlides(); // Call the function to run the slideshow
