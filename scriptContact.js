7ffaa6117dbc58e0ce5da633f9d6049f0509384b
Tink <85289337+tink012184@users.noreply.github.com>
2024-04-12 13:14:17 -0600

    texting

--- scriptContact.js
+++ scriptContact.js
@@ -27,7 +27,7 @@ document.getElementById('contactForm').addEventListener('submit', function(event
 
 function sendEmail() {
     const recipientEmail = 'tink012184@icloud.com'; // Replace with the actual recipient's email
-    const senderEmail = document.getElementById("email").value; // Replace with the sender's email
+    const senderEmail = 'tink012184@hotmail.com';//document.getElementById("email").value; // Replace with the sender's email
     const subject = 'KJ/Trivia Form Submitted';
     const name = document.getElementById('name').value;
     const message = document.getElementById('message').value; // Get the user's input from the form
